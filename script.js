d0dbbd062ff8c5ec3ba0256a56802857415dcfb4
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-29 22:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 const otp=document.querySelectorAll(".code");
-
+// otp[0].focus();
 function handleInput() {
 	let val=this.event.target.value;
-	let index=parseInt(this.event.target.id.split("-")[1]);
+	let index=parseInt(this.event.target.id.split("-")[1]) -1;
 	 
 	if(val>=0 && val<=9 && index<=4){
 		otp[index+1].focus();
